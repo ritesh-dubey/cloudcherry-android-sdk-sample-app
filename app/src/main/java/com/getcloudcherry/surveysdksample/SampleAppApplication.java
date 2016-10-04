92b1644708d238c6d0cca3fae69199838635ed5a
Madhur Tewani <madhur.tewani@wowlabz.com>
2016-10-04 19:23:31 +0530

    * Initialise SDK with static token or dynamic token based on selection. * Initialise SDK with assets for smiley rating and star rating. * Initialise SDK with style for custom text.

--- app/src/main/java/com/getcloudcherry/surveysdksample/SampleAppApplication.java
+++ app/src/main/java/com/getcloudcherry/surveysdksample/SampleAppApplication.java
@@ -2,9 +2,6 @@ package com.getcloudcherry.surveysdksample;
 
 import android.app.Application;
 
-import com.getcloudcherry.survey.helper.SurveyCC;
-import com.getcloudcherry.survey.model.SurveyToken;
-
 /**
  * Created by riteshdubey on 8/8/16.
  */
@@ -12,7 +9,5 @@ public class SampleAppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SurveyToken aTokenConfig = new SurveyToken(1, "mobile");
-        SurveyCC.initialise(this, "rohith", "Test@123", aTokenConfig);
     }
 }
